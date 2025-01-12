249699d1daf02d6350c85cf4c9194c0f93c4d857
Grober Gonzales <GonzalesDLCGrober@gmail.com>
2025-01-12 16:08:17 -0500

    Clients Section

---
+++ src/constants/clients.constants.ts
@@ -0,0 +1,17 @@
+import client1 from '@/assets/logos/client1.svg';
+import client2 from '@/assets/logos/client2.svg';
+import client3 from '@/assets/logos/client3.svg';
+import client4 from '@/assets/logos/client4.svg';
+import client5 from '@/assets/logos/client5.svg';
+import client6 from '@/assets/logos/client6.svg';
+import client7 from '@/assets/logos/client7.svg';
+
+export const clientList: Array<{ src: ImageMetadata; alt: string }> = [
+  { src: client1, alt: 'Client 1' },
+  { src: client2, alt: 'Client 2' },
+  { src: client3, alt: 'Client 3' },
+  { src: client4, alt: 'Client 4' },
+  { src: client5, alt: 'Client 5' },
+  { src: client6, alt: 'Client 6' },
+  { src: client7, alt: 'Client 7' },
+];
